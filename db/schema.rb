e9b4ccbbdb8b9fe2bb67037372f7f97202f5b771
vinyasbj <vinyas555@gmail.com>
2017-06-10 17:50:34 +0530

    added subscriber feature

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610115622) do
+ActiveRecord::Schema.define(version: 20170610121849) do
 
   create_table "advertisers", force: true do |t|
     t.string   "name"
@@ -43,6 +43,12 @@ ActiveRecord::Schema.define(version: 20170610115622) do
     t.string   "title"
     t.text     "body"
     t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "subscribers", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
